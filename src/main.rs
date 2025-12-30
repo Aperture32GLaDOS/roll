7a308cf4966f59367da7097769cf619f510bddbf
Eshe <ehinchliffe@proton.me>
2025-12-30 19:47:43 +0000

    Added new flag to control if the output should be long or short

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::{command, Arg};
-use rand::Rng;
+use clap::{command, Arg, ArgAction};
+use rand::{rngs::ThreadRng, Rng};
 
 #[derive(Debug)]
 struct Roll {
@@ -37,8 +37,162 @@ enum DropDie {
     DropHighest(usize)
 }
 
+
+// Add to a running total and build up an output string
+fn add_to_total_and_output(index: usize, roll: &mut Roll, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool) -> Result<(), Box<dyn std::error::Error>> {
+    // Roll the dice
+    if roll.num_rolls == 0 {
+        return Ok(());
+    }
+    roll.results.reserve(roll.num_rolls);
+    for _ in 0..roll.num_rolls {
+        roll.results.push(rng.random_range(1..=roll.dice_sides).into())
+    }
+    if index > 0 {
+        match roll.multiplier {
+            1 => {
+                *output += " + ";
+            }
+            -1 => {
+                *output += " - ";
+            }
+            _ => {
+                return Err("Multiplier somehow obtained as neither 1 or -1".into());
+            }
+        }
+    }
+    if roll.num_rolls == 1 {
+        *output += &roll.results.first().unwrap().to_string();
+    }
+    // If we are dropping, then...
+    match &roll.drop_die {
+        Some(y) => {
+            let drop_n: usize;
+            match y {
+                DropDie::DropLowest(x) => {
+                    if *x >= roll.num_rolls {
+                        return Ok(());
+                    }
+                    roll.results.sort_by_key(|n| *n);
+                    drop_n = *x;
+                    
+                }
+                DropDie::DropHighest(x) => {
+                    if *x >= roll.num_rolls {
+                        return Ok(());
+                    }
+                    roll.results.sort_by_key(|n| *n);
+                    roll.results.reverse();
+                    drop_n = *x;
+                }
+            }
+            // If skipping,
+            if skip_dropped {
+                // Start by removing the dropped rolls
+                roll.results.drain(0..drop_n);
+                // And then add to output string
+                if roll.results.len() > 1 {
+                    output.push('('); 
+                    for (result_index, result) in roll.results.iter().enumerate() {
+                        if result_index > 0 {
+                            *output += " + "
+                        }
+                        *output += &result.to_string();
+                    }
+                    output.push(')');
+                }
+                else {
+                    for (result_index, result) in roll.results.iter().enumerate() {
+                        if result_index > 0 {
+                            *output += " + "
+                        }
+                        *output += &result.to_string();
+                    }
+                }
+            }
+            // If not skipping,
+            else {
+                // Start with adding to output string
+                if roll.num_rolls > 1 && !skip_dropped {
+                    output.push('('); 
+                    for (result_index, result) in roll.results.iter().enumerate() {
+                        if result_index > 0 {
+                            *output += " + "
+                        }
+                        if result_index < drop_n {
+                            *output += "\x1b[0;91mX\x1b[0m";
+                        }
+                        *output += &result.to_string();
+                    }
+                    output.push(')');
+                }
+                // And end with removing the dropped dice
+                roll.results.drain(0..drop_n);
+            }
+        }
+        None => {
+            if roll.num_rolls > 1 {
+                output.push('('); 
+                for (result_index, result) in roll.results.iter().enumerate() {
+                    if result_index > 0 {
+                        *output += " + "
+                    }
+                    *output += &result.to_string();
+                }
+                output.push(')');
+            }
+        }
+    }
+    *total += ((roll.multiplier as i32) * (roll.results.iter().sum::<u32>() as i32)) as i64;
+    Ok(())
+}
+
+// Only add to the running total, doing no string manipulation
+fn add_to_total(roll: &mut Roll, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
+    // Roll the dice
+    if roll.num_rolls == 0 {
+        return Ok(());
+    }
+    roll.results.reserve(roll.num_rolls);
+    for _ in 0..roll.num_rolls {
+        roll.results.push(rng.random_range(1..=roll.dice_sides).into())
+    }
+    // If we are dropping, then...
+    match &roll.drop_die {
+        Some(y) => {
+            let drop_n: usize;
+            match y {
+                DropDie::DropLowest(x) => {
+                    if *x >= roll.num_rolls {
+                        return Ok(());
+                    }
+                    roll.results.sort_by_key(|n| *n);
+                    drop_n = *x;
+                    
+                }
+                DropDie::DropHighest(x) => {
+                    if *x >= roll.num_rolls {
+                        return Ok(());
+                    }
+                    roll.results.sort_by_key(|n| *n);
+                    roll.results.reverse();
+                    drop_n = *x;
+                }
+            }
+            // And end with removing the dropped dice
+            roll.results.drain(0..drop_n);
+        }
+        None => {}
+    }
+    *total += ((roll.multiplier as i32) * (roll.results.iter().sum::<u32>() as i32)) as i64;
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut command = command!().arg(Arg::new("dice").num_args(1..).help("The dice to be rolled - syntax is NdN, and many dice may be summed or subtracted.\nIt is also possible to drop the N lowest or highest results, i.e. with 2d20dl1 or 2d20dh1. The dropped dice will be marked in \x1b[0;91mred\x1b[0m"));
+    let mut command = command!()
+        .arg(Arg::new("dice").num_args(1..).help("The dice to be rolled - syntax is NdN, and many dice may be summed or subtracted.\nIt is also possible to drop the N lowest or highest results, i.e. with 2d20dl1 or 2d20dh1. The dropped dice will be marked in \x1b[0;91mred\x1b[0m"))
+        .arg(Arg::new("skip-dropped").short('s').long("skip-dropped").action(ArgAction::SetTrue).help("Do not show dice which have been dropped i.e. in 2d20dl1"))
+        .arg(Arg::new("short-output").long("short-output").action(ArgAction::SetTrue).help("Only show the result of the rolls"));
     command.build();
     let help_message = command.render_long_help();
     let matches = command.get_matches();
@@ -52,6 +206,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{}", help_message);
         return Ok(());
     }
+    let skip_dropped = matches.get_flag("skip-dropped");
+    let short_output = matches.get_flag("short-output");
     let mut state: States = States::ObtainingNumberOfDice;
     let mut rolls: Vec<Roll> = Vec::new();
     let mut new_roll = Roll::new();
@@ -185,94 +341,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // And then we build the output
     let mut output: String = String::new();
     for (index, roll) in rolls.iter_mut().enumerate() {
-        // Roll the dice
-        if roll.num_rolls == 0 {
-            continue;
+        if short_output {
+            add_to_total(roll, &mut running_total, &mut rng)?;
         }
-        roll.results.reserve(roll.num_rolls);
-        for _ in 0..roll.num_rolls {
-            roll.results.push(rng.random_range(1..=roll.dice_sides).into())
+        else {
+            add_to_total_and_output(index, roll, &mut running_total, &mut output, &mut rng, skip_dropped)?;
         }
-        if index > 0 {
-            match roll.multiplier {
-                1 => {
-                    output += " + ";
-                }
-                -1 => {
-                    output += " - ";
-                }
-                _ => {
-                    return Err("Multiplier somehow obtained as neither 1 or -1".into());
-                }
-            }
-        }
-        if roll.num_rolls == 1 {
-            output += &roll.results.first().unwrap().to_string();
-        }
-        // If we are dropping, then...
-        match &roll.drop_die {
-            Some(y) => {
-                match y {
-                    DropDie::DropLowest(x) => {
-                        if *x >= roll.num_rolls {
-                            continue;
-                        }
-                        roll.results.sort_by_key(|n| *n);
-                        if roll.num_rolls > 1 {
-                            output.push('('); 
-                            for (result_index, result) in roll.results.iter().enumerate() {
-                                if result_index > 0 {
-                                    output += " + "
-                                }
-                                if result_index < *x {
-                                    output += "\x1b[0;91mX\x1b[0m";
-                                }
-                                output += &result.to_string();
-                            }
-                            output.push(')');
-                        }
-                        roll.results.drain(0..*x);
-                    }
-                    DropDie::DropHighest(x) => {
-                        if *x >= roll.num_rolls {
-                            continue;
-                        }
-                        roll.results.sort_by_key(|n| *n);
-                        roll.results.reverse();
-                        if roll.num_rolls > 1 {
-                            output.push('('); 
-                            for (result_index, result) in roll.results.iter().enumerate() {
-                                if result_index > 0 {
-                                    output += " + "
-                                }
-                                if result_index < *x {
-                                    output += "\x1b[0;91mX\x1b[0m";
-                                }
-                                output += &result.to_string();
-                            }
-                            output.push(')');
-                        }
-                        roll.results.drain(0..*x);
-                    }
-                }
-            }
-            None => {
-                if roll.num_rolls > 1 {
-                    output.push('('); 
-                    for (result_index, result) in roll.results.iter().enumerate() {
-                        if result_index > 0 {
-                            output += " + "
-                        }
-                        output += &result.to_string();
-                    }
-                    output.push(')');
-                }
-            }
-        }
-        running_total += ((roll.multiplier as i32) * (roll.results.iter().sum::<u32>() as i32)) as i64;
     }
     // Specify what the result of our rolls were
     output += &(" => ".to_owned() + &running_total.to_string());
-    println!("{}", output);
+    if short_output {
+        println!("{}", running_total);
+    }
+    else {
+        println!("{}", output);
+    }
     Ok(())
 }
