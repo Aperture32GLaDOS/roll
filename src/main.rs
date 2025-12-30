9ff57cc94c2643019d96f9c714bd79ea1aa61be8
Eshe <ehinchliffe@proton.me>
2025-12-30 19:58:34 +0000

    Added about

--- src/main.rs
+++ src/main.rs
@@ -192,7 +192,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut command = command!()
         .arg(Arg::new("dice").num_args(1..).help("The dice to be rolled - syntax is NdN, and many dice may be summed or subtracted.\nIt is also possible to drop the N lowest or highest results, i.e. with 2d20dl1 or 2d20dh1. The dropped dice will be marked in \x1b[0;91mred\x1b[0m"))
         .arg(Arg::new("skip-dropped").short('s').long("skip-dropped").action(ArgAction::SetTrue).help("Do not show dice which have been dropped i.e. in 2d20dl1"))
-        .arg(Arg::new("short-output").long("short-output").action(ArgAction::SetTrue).help("Only show the result of the rolls"));
+        .arg(Arg::new("short-output").long("short-output").action(ArgAction::SetTrue).help("Only show the result of the rolls"))
+        .long_about("Rolls dice for use in D&D");
     command.build();
     let help_message = command.render_long_help();
     let matches = command.get_matches();
