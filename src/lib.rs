381713a3bfe5472a0c45464dc93ddf98a328f664
Eshe <ehinchliffe@proton.me>
2026-06-28 13:42:34 +0100

    Added the capability to handle constant (i.e. 1d20 + 5)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use rand::{distr::Uniform, rngs::ThreadRng, Rng};
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
+pub enum RollOrConstant {
+    ROLL(Roll),
+    CONST(ConstantRoll)
+}
+
 #[derive(Debug)]
 pub struct Roll {
     // The number of dice to be rolled
@@ -17,9 +22,225 @@ pub struct Roll {
     pub results: Vec<u32>
 }
 
+pub struct ConstantRoll {
+    pub multiplier: i8,
+    pub constant_result: i64
+}
+
 impl Roll {
     pub fn new() -> Self {
         Roll { num_rolls: 0, dice_sides: 0, multiplier: 1, drop_die: None, results: Vec::new() }
+    }
+}
+
+impl ConstantRoll {
+    pub fn new() -> Self {
+        ConstantRoll { multiplier: 1, constant_result: 0 }
+    }
+}
+
+pub trait Rollable {
+    // Add to a running total and build up an output string
+    fn roll_with_output(&mut self, index: usize, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool, colour: bool) -> Result<(), Box<dyn std::error::Error>>;
+    fn roll(&mut self, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+impl Rollable for ConstantRoll {
+    fn roll_with_output(&mut self, index: usize, total: &mut i64, output: &mut String, _rng: &mut ThreadRng, _skip_dropped: bool, _colour: bool) -> Result<(), Box<dyn std::error::Error>> {
+        if index > 0 {
+            match self.multiplier {
+                1 => {
+                    *output += " + ";
+                }
+                -1 => {
+                    *output += " - ";
+                }
+                _ => {
+                    return Err("Multiplier somehow obtained as neither 1 or -1".into());
+                }
+            }
+        }
+        *output += &self.constant_result.to_string();
+        *total += self.constant_result * (self.multiplier as i64);
+        Ok(())
+    }
+
+    fn roll(&mut self, total: &mut i64, _rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
+        *total += self.constant_result * (self.multiplier as i64);
+        Ok(())
+    }
+}
+
+impl Rollable for Roll {
+    fn roll_with_output(&mut self, index: usize, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool, colour: bool) -> Result<(), Box<dyn std::error::Error>> {
+        // Roll the dice
+        if self.num_rolls == 0 {
+            return Ok(());
+        }
+        self.results.reserve(self.num_rolls);
+        let distribution = Uniform::new_inclusive(1, self.dice_sides)?;
+        for _ in 0..self.num_rolls {
+            self.results.push(rng.sample(distribution).into())
+        }
+        if index > 0 {
+            match self.multiplier {
+                1 => {
+                    *output += " + ";
+                }
+                -1 => {
+                    *output += " - ";
+                }
+                _ => {
+                    return Err("Multiplier somehow obtained as neither 1 or -1".into());
+                }
+            }
+        }
+        if self.num_rolls == 1 {
+            *output += &self.results.first().unwrap().to_string();
+        }
+        // If we are dropping, then...
+        match &self.drop_die {
+            Some(y) => {
+                let drop_n: usize;
+                match y {
+                    DropDie::DropLowest(x) => {
+                        if *x >= self.num_rolls {
+                            return Ok(());
+                        }
+                        self.results.sort_by_key(|n| *n);
+                        drop_n = *x;
+                        
+                    }
+                    DropDie::DropHighest(x) => {
+                        if *x >= self.num_rolls {
+                            return Ok(());
+                        }
+                        self.results.sort_by_key(|n| *n);
+                        self.results.reverse();
+                        drop_n = *x;
+                    }
+                }
+                // If skipping,
+                if skip_dropped {
+                    // Start by removing the dropped rolls
+                    self.results.drain(0..drop_n);
+                    // And then add to output string
+                    if self.results.len() > 1 {
+                        output.push('('); 
+                        for (result_index, result) in self.results.iter().enumerate() {
+                            if result_index > 0 {
+                                *output += " + "
+                            }
+                            *output += &result.to_string();
+                        }
+                        output.push(')');
+                    }
+                    else {
+                        for (result_index, result) in self.results.iter().enumerate() {
+                            if result_index > 0 {
+                                *output += " + "
+                            }
+                            *output += &result.to_string();
+                        }
+                    }
+                }
+                // If not skipping,
+                else {
+                    // Start with adding to output string
+                    if self.num_rolls > 1 && !skip_dropped {
+                        output.push('('); 
+                        for (result_index, result) in self.results.iter().enumerate() {
+                            if result_index > 0 {
+                                *output += " + "
+                            }
+                            if result_index < drop_n {
+                                if colour {
+                                    *output += "\x1b[0;91mX\x1b[0m";
+                                }
+                                else {
+                                    *output += "X";
+                                }
+                            }
+                            *output += &result.to_string();
+                        }
+                        output.push(')');
+                    }
+                    // And end with removing the dropped dice
+                    self.results.drain(0..drop_n);
+                }
+            }
+            None => {
+                if self.num_rolls > 1 {
+                    output.push('('); 
+                    for (result_index, result) in self.results.iter().enumerate() {
+                        if result_index > 0 {
+                            *output += " + "
+                        }
+                        *output += &result.to_string();
+                    }
+                    output.push(')');
+                }
+            }
+        }
+        *total += ((self.multiplier as i32) * (self.results.iter().sum::<u32>() as i32)) as i64;
+        Ok(())
+    }
+
+    fn roll(&mut self, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
+        // Roll the dice
+        if self.num_rolls == 0 {
+            return Ok(());
+        }
+        self.results.reserve(self.num_rolls);
+        let distribution = Uniform::new_inclusive(1, self.dice_sides)?;
+        for _ in 0..self.num_rolls {
+            self.results.push(rng.sample(distribution).into())
+        }
+        // If we are dropping, then...
+        match &self.drop_die {
+            Some(y) => {
+                let drop_n: usize;
+                match y {
+                    DropDie::DropLowest(x) => {
+                        if *x >= self.num_rolls {
+                            return Ok(());
+                        }
+                        self.results.sort_by_key(|n| *n);
+                        drop_n = *x;
+                        
+                    }
+                    DropDie::DropHighest(x) => {
+                        if *x >= self.num_rolls {
+                            return Ok(());
+                        }
+                        self.results.sort_by_key(|n| *n);
+                        self.results.reverse();
+                        drop_n = *x;
+                    }
+                }
+                // And end with removing the dropped dice
+                self.results.drain(0..drop_n);
+            }
+            None => {}
+        }
+        *total += ((self.multiplier as i32) * (self.results.iter().sum::<u32>() as i32)) as i64;
+        Ok(())
+    }
+}
+
+impl Rollable for RollOrConstant {
+    fn roll(&mut self, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
+        match self {
+            Self::ROLL(x) => x.roll(total, rng),
+            Self::CONST(x) => x.roll(total, rng)
+        }
+    }
+
+    fn roll_with_output(&mut self, index: usize, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool, colour: bool) -> Result<(), Box<dyn std::error::Error>> {
+        match self {
+            Self::ROLL(x) => x.roll_with_output(index, total, output, rng, skip_dropped, colour),
+            Self::CONST(x) => x.roll_with_output(index, total, output, rng, skip_dropped, colour)
+        }
     }
 }
 
@@ -39,168 +260,27 @@ pub enum DropDie {
 }
 
 
-// Add to a running total and build up an output string
-pub fn add_to_total_and_output(index: usize, roll: &mut Roll, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool, colour: bool) -> Result<(), Box<dyn std::error::Error>> {
-    // Roll the dice
-    if roll.num_rolls == 0 {
-        return Ok(());
-    }
-    roll.results.reserve(roll.num_rolls);
-    let distribution = Uniform::new_inclusive(1, roll.dice_sides)?;
-    for _ in 0..roll.num_rolls {
-        roll.results.push(rng.sample(distribution).into())
-    }
-    if index > 0 {
-        match roll.multiplier {
-            1 => {
-                *output += " + ";
-            }
-            -1 => {
-                *output += " - ";
-            }
-            _ => {
-                return Err("Multiplier somehow obtained as neither 1 or -1".into());
-            }
-        }
-    }
-    if roll.num_rolls == 1 {
-        *output += &roll.results.first().unwrap().to_string();
-    }
-    // If we are dropping, then...
-    match &roll.drop_die {
-        Some(y) => {
-            let drop_n: usize;
-            match y {
-                DropDie::DropLowest(x) => {
-                    if *x >= roll.num_rolls {
-                        return Ok(());
-                    }
-                    roll.results.sort_by_key(|n| *n);
-                    drop_n = *x;
-                    
-                }
-                DropDie::DropHighest(x) => {
-                    if *x >= roll.num_rolls {
-                        return Ok(());
-                    }
-                    roll.results.sort_by_key(|n| *n);
-                    roll.results.reverse();
-                    drop_n = *x;
-                }
-            }
-            // If skipping,
-            if skip_dropped {
-                // Start by removing the dropped rolls
-                roll.results.drain(0..drop_n);
-                // And then add to output string
-                if roll.results.len() > 1 {
-                    output.push('('); 
-                    for (result_index, result) in roll.results.iter().enumerate() {
-                        if result_index > 0 {
-                            *output += " + "
-                        }
-                        *output += &result.to_string();
-                    }
-                    output.push(')');
-                }
-                else {
-                    for (result_index, result) in roll.results.iter().enumerate() {
-                        if result_index > 0 {
-                            *output += " + "
-                        }
-                        *output += &result.to_string();
-                    }
-                }
-            }
-            // If not skipping,
-            else {
-                // Start with adding to output string
-                if roll.num_rolls > 1 && !skip_dropped {
-                    output.push('('); 
-                    for (result_index, result) in roll.results.iter().enumerate() {
-                        if result_index > 0 {
-                            *output += " + "
-                        }
-                        if result_index < drop_n {
-                            if colour {
-                                *output += "\x1b[0;91mX\x1b[0m";
-                            }
-                            else {
-                                *output += "X";
-                            }
-                        }
-                        *output += &result.to_string();
-                    }
-                    output.push(')');
-                }
-                // And end with removing the dropped dice
-                roll.results.drain(0..drop_n);
-            }
-        }
-        None => {
-            if roll.num_rolls > 1 {
-                output.push('('); 
-                for (result_index, result) in roll.results.iter().enumerate() {
-                    if result_index > 0 {
-                        *output += " + "
-                    }
-                    *output += &result.to_string();
-                }
-                output.push(')');
-            }
-        }
-    }
-    *total += ((roll.multiplier as i32) * (roll.results.iter().sum::<u32>() as i32)) as i64;
-    Ok(())
+pub fn add_to_total_and_output<T: Rollable>(index: usize, rollable: &mut T, total: &mut i64, output: &mut String, rng: &mut ThreadRng, skip_dropped: bool, colour: bool) -> Result<(), Box<dyn std::error::Error>> {
+    rollable.roll_with_output(index, total, output, rng, skip_dropped, colour)
 }
 
 // Only add to the running total, doing no string manipulation
-pub fn add_to_total(roll: &mut Roll, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
-    // Roll the dice
-    if roll.num_rolls == 0 {
-        return Ok(());
-    }
-    roll.results.reserve(roll.num_rolls);
-    let distribution = Uniform::new_inclusive(1, roll.dice_sides)?;
-    for _ in 0..roll.num_rolls {
-        roll.results.push(rng.sample(distribution).into())
-    }
-    // If we are dropping, then...
-    match &roll.drop_die {
-        Some(y) => {
-            let drop_n: usize;
-            match y {
-                DropDie::DropLowest(x) => {
-                    if *x >= roll.num_rolls {
-                        return Ok(());
-                    }
-                    roll.results.sort_by_key(|n| *n);
-                    drop_n = *x;
-                    
-                }
-                DropDie::DropHighest(x) => {
-                    if *x >= roll.num_rolls {
-                        return Ok(());
-                    }
-                    roll.results.sort_by_key(|n| *n);
-                    roll.results.reverse();
-                    drop_n = *x;
-                }
-            }
-            // And end with removing the dropped dice
-            roll.results.drain(0..drop_n);
-        }
-        None => {}
-    }
-    *total += ((roll.multiplier as i32) * (roll.results.iter().sum::<u32>() as i32)) as i64;
-    Ok(())
+pub fn add_to_total<T: Rollable>(rollable: &mut T, total: &mut i64, rng: &mut ThreadRng) -> Result<(), Box<dyn std::error::Error>> {
+    rollable.roll(total, rng)
 }
 
-pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<Vec<Roll>, Box<dyn std::error::Error>> {
-    let mut rolls: Vec<Roll> = Vec::new();
+pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<Vec<RollOrConstant>, Box<dyn std::error::Error>> {
+    let mut rolls: Vec<RollOrConstant> = Vec::new();
     let mut new_roll = Roll::new();
+    let mut new_const = ConstantRoll::new();
     let mut state: States = States::ObtainingNumberOfDice;
-    for character in input.trim().chars() {
+    let mut is_const: bool = false;
+    let mut index: usize = 0;
+    let trimmed_input = input.trim().chars().collect::<Vec<char>>();
+    // While loop so we can go back and forth between characters in our FSM
+    while index < trimmed_input.len() {
+        let character = trimmed_input[index];
+        index += 1;
         // Depending on our current state,
         match state {
             States::ObtainingDiceSides => {
@@ -212,18 +292,18 @@ pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<V
                     match character {
                         ' ' => {
                             state = States::ObtainingNextOperation;
-                            rolls.push(new_roll);
+                            rolls.push(RollOrConstant::ROLL(new_roll));
                             new_roll = Roll::new();
                         }
                         '+' => {
                             state = States::ObtainingNumberOfDice;
-                            rolls.push(new_roll);
+                            rolls.push(RollOrConstant::ROLL(new_roll));
                             new_roll = Roll::new();
                         }
                         '-' => {
                             new_roll.multiplier = -1;
                             state = States::ObtainingNumberOfDice;
-                            rolls.push(new_roll);
+                            rolls.push(RollOrConstant::ROLL(new_roll));
                             new_roll = Roll::new();
                         }
                         'd' => {
@@ -253,9 +333,17 @@ pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<V
                         ' ' => {
                             continue;
                         }
+                        // If we find a character we are not expecting, assume that this is a
+                        // constant and parse the current character as part of the next operation
                         _ => {
-                            println!("{}", help_message);
-                            return Err("Unknown character encountered".into());
+                            new_const.multiplier = new_roll.multiplier;
+                            new_const.constant_result = new_roll.num_rolls as i64;
+                            rolls.push(RollOrConstant::CONST(new_const));
+                            new_const = ConstantRoll::new();
+                            new_roll = Roll::new();
+                            state = States::ObtainingNextOperation;
+                            index -= 1;
+                            continue;
                         }
                     }
                 }
@@ -297,9 +385,16 @@ pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<V
                     }
                 }
                 else {
-                    rolls.push(new_roll);
-                    new_roll = Roll::new();
+                    if is_const {
+                        rolls.push(RollOrConstant::CONST(new_const));
+                        new_const = ConstantRoll::new();
+                    }
+                    else {
+                        rolls.push(RollOrConstant::ROLL(new_roll));
+                        new_roll = Roll::new();
+                    }
                     state = States::ObtainingNextOperation;
+                    is_const = false;
                 }
             }
 
@@ -323,7 +418,12 @@ pub fn consume_input_to_rolls(input: &String, help_message: &String) -> Result<V
             }
         }
     }
-    rolls.push(new_roll);
+    if new_roll.dice_sides == 0 {
+        rolls.push(RollOrConstant::CONST(ConstantRoll { multiplier: new_roll.multiplier, constant_result: new_roll.num_rolls as i64 }))
+    }
+    else {
+        rolls.push(RollOrConstant::ROLL(new_roll));
+    }
     Ok(rolls)
 }
 
